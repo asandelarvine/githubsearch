a18c5ac6d80adf95046b97d1b5945cdc8348e1a4
asandelarvine <asandelarvine@gmail.com>
2021-10-03 00:49:59 -0800

    define repo-handler: retrieve repo-list for a user

--- src/app/github-user/github-user.component.ts
+++ src/app/github-user/github-user.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { GitserviceService } from '../services/gitservice.service';
 import { User } from '../user';
+import { Repository } from '../repository';
 
 @Component({
   selector: 'app-github-user',
@@ -11,14 +12,24 @@ import { User } from '../user';
 })
 export class GithubUserComponent implements OnInit {
   user!: User;
+  repo:Repository;
 
   constructor(private service: GitserviceService) {
+    this.repo = new Repository('','','',);
 
   }
   search(typedWord: any) {
     this.service.find(typedWord).then((success) => {
       this.user = this.service.user
+      console.log(this.user);
     })
+    this.service.repoHandler(typedWord).then((success) => {
+      this.repo = this.service.repo;
+      console.log(this.repo);
+    },
+    (error) => {
+      console.log(error)
+    });
   }
 
   ngOnInit(): void {
--- src/app/services/gitservice.service.ts
+++ src/app/services/gitservice.service.ts
@@ -4,15 +4,18 @@ import { environment } from 'src/environments/environment';
 
 
 import { User } from '../user';
+import { Repository } from '../repository';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GitserviceService {
-  user!: User;
+  user: User;
+  repo:Repository;
 
   constructor(private http: HttpClient) {
-    this.user = new User('', '', '', 0, 0, 0,)
+    this.user = new User('', '', '', 0, 0, 0,);
+    this.repo = new Repository('','','',);
   }
   find(typedWord: any) {
     interface userApi {
@@ -24,10 +27,10 @@ export class GitserviceService {
       following: number,
     }
     let headerExtras = new HttpHeaders({ 'Authorization': 'token ' + environment.pass })
-    let searchFor = environment.baseUrl + typedWord;
-    let addToRequest= { headers: headerExtras }
+    let url = environment.baseUrl + typedWord;
+    let addToRequest = { headers: headerExtras }
     let promise = new Promise((resolve, reject) => {
-      this.http.get<userApi>(searchFor, addToRequest).toPromise().then((response) => {
+      this.http.get<userApi>(url, addToRequest).toPromise().then((response) => {
         this.user = response;
 
         resolve(resolve);
@@ -40,5 +43,27 @@ export class GitserviceService {
       );
     });
     return promise;
+  }
+  repoHandler(user: any) {
+    interface repoApi {
+      html_url: string,
+      language: string,
+      description: string,
+    }
+    let headerExtras = new HttpHeaders({ 'Authorization': 'token' + environment.pass })
+    let addToRequest = { headers: headerExtras }
+    let url = environment.baseUrl + user + '/repos';
+    let promise = new Promise((resolve, reject) => {
+      this.http.get<repoApi>(url, addToRequest).toPromise().then(response => {
+        this.repo = response;
+        resolve(resolve)
+
+      }, error => {
+        reject();
+        console.log(error)
+      });
+
+    });
+    return promise
   }
 }
